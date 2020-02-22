56dbc8a01d6827558adca87c4bca23fc5cfc62d5
Matteo Fenu <matfen97@gmail.com>
2020-02-22 13:24:42 +0100

    fixed index.js to handle production

--- server/index.js
+++ server/index.js
@@ -13,6 +13,14 @@ app.use(express.urlencoded({ extended: true }))
 const todos = require('./routes/api/todos')
 app.use('/api/todos', todos)
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public/'))
+    // Handle SPA 
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 // Server start
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`The app is running on port ${port}`))
